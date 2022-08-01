2ed28bd40c46dfc861f6d5c47f53197d02224da7
ck-lamib <113ck.lamib@gmail.com>
2022-08-01 22:57:55 +0545

    adding materialapp, scaffold, and elevatedButton from video 6

--- first_flutter_app/lib/main.dart
+++ first_flutter_app/lib/main.dart
@@ -6,83 +6,102 @@ void main(List<String> args) {
 
 class App extends StatelessWidget {
   @override
+  Widget build(BuildContext contex) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        appBarTheme: AppBarTheme(
+          backgroundColor: Color(0xff0a0e21),
+        ),
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(20),
-      child: Column(
-        children: [
-          Container(
-            color: Colors.white,
-            padding: EdgeInsets.all(10),
-            margin: EdgeInsets.only(bottom: 10),
-            child: const Text(
-              "Where am I?",
-              textDirection: TextDirection.ltr,
-              style: TextStyle(
-                  color: Colors.pink,
-                  fontSize: 40,
-                  fontWeight: FontWeight.bold,
-                  backgroundColor: Colors.amber,
-                  fontStyle: FontStyle.italic),
-            ),
-          ),
-          Container(
-            width: 200,
-            color: Colors.grey.shade400,
-            padding: EdgeInsets.all(10),
-            margin: EdgeInsets.only(bottom: 10),
-            child: const Text(
-              "1. Baglung",
-              textDirection: TextDirection.ltr,
-            ),
-          ),
-          Container(
-            width: 200,
-            color: Colors.grey.shade400,
-            padding: EdgeInsets.all(10),
-            margin: EdgeInsets.only(bottom: 10),
-            child: const Text(
-              "2. Kathmandu",
-              textDirection: TextDirection.ltr,
-            ),
-          ),
-          Container(
-            width: 200,
-            color: Colors.grey.shade400,
-            padding: EdgeInsets.all(10),
-            margin: EdgeInsets.only(bottom: 10),
-            child: const Text(
-              "3. Pokhara",
-              textDirection: TextDirection.ltr,
-            ),
-          ),
-          Container(
-            width: 200,
-            color: Colors.grey.shade400,
-            padding: EdgeInsets.all(10),
-            margin: EdgeInsets.only(bottom: 10),
-            child: const Text(
-              "4. Birjung",
-              textDirection: TextDirection.ltr,
-            ),
-          ),
-          GestureDetector(
-            onTap: (() {
-              print("submit button is pressed.");
-            }),
-            child: Container(
-              width: 100,
-              color: Colors.red,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Flutter app"),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              color: Colors.white,
+              width: double.maxFinite,
               padding: EdgeInsets.all(10),
-              margin: EdgeInsets.only(top: 10),
-              alignment: Alignment.center,
+              margin: EdgeInsets.only(bottom: 10),
+              child: const Text(
+                "Where am I?",
+                textDirection: TextDirection.ltr,
+                style: TextStyle(
+                    color: Colors.pink,
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                    backgroundColor: Colors.amber,
+                    fontStyle: FontStyle.italic),
+              ),
+            ),
+            TextButton(
+              onPressed: () => print("option 1 is selected"),
+              style: TextButton.styleFrom(
+                fixedSize: Size(150, 60),
+                alignment: Alignment.centerLeft,
+              ),
+              child: const Text(
+                "1. Baglung",
+                textDirection: TextDirection.ltr,
+              ),
+            ),
+            TextButton(
+              onPressed: () => print("option 2 is selected"),
+              style: TextButton.styleFrom(
+                fixedSize: Size(150, 60),
+                alignment: Alignment.centerLeft,
+              ),
+              child: const Text(
+                "2. Kathmandu",
+                textDirection: TextDirection.ltr,
+              ),
+            ),
+            TextButton(
+              onPressed: () => print("option 3 is selected"),
+              style: TextButton.styleFrom(
+                fixedSize: Size(150, 60),
+                alignment: Alignment.centerLeft,
+              ),
+              child: const Text(
+                "3. Pokhara",
+                textDirection: TextDirection.ltr,
+              ),
+            ),
+            TextButton(
+              onPressed: () => print("option 4 is selected"),
+              style: TextButton.styleFrom(
+                fixedSize: Size(150, 60),
+                alignment: Alignment.centerLeft,
+              ),
+              child: const Text(
+                "4. Birjung",
+                textDirection: TextDirection.ltr,
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                print("submit button is pressed.");
+              },
+              style: ElevatedButton.styleFrom(fixedSize: Size(150, 60)),
               child: const Text(
                 "Button",
                 textDirection: TextDirection.ltr,
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
