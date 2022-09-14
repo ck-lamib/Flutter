32ba1fbe53c09d42b843c30d458af000fb41acdb
ck-lamib <113ck.lamib@gmail.com>
2022-09-14 12:00:32 +0545

    first commit: creating Simple Calculator

---
+++ calculator_app/lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Simple Calculator",
+      home: CalculatorApp(),
+    );
+  }
+}
+
+class CalculatorApp extends StatefulWidget {
+  const CalculatorApp({super.key});
+
+  @override
+  State<CalculatorApp> createState() => _CalculatorAppState();
+}
+
+class _CalculatorAppState extends State<CalculatorApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Simple Calculator"),
+      ),
+    );
+  }
+}
